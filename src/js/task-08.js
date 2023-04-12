ad608b0891ea79b902b914364b39defc4fe5e4c9
Mykyta Pokasiuk <mykyta.pokasiuk@ukr.net>
2023-04-12 23:30:41 +0300

    Task-08 is completed

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,38 @@
-
+const form = {
+    formElement: document.querySelector(".login-form"),
+    /**
+     * Creates object
+     * @param {string} email
+     * @param {string} password
+     */
+    UserInfo: function (email, password) {
+        (this.email = email), (this.password = password);
+    },
+    /**
+     * Logs new object to the console created by UserInfo(constructor)
+     */
+    logUserInfo() {
+        const {
+            elements: { email, password },
+        } = this.formElement;
+        const userInfo = new this.UserInfo(email.value, password.value);
+        console.log(userInfo);
+    },
+    /**
+     * Checks, if all the form fields was filled, fires logUserInfo method, clears all form fields
+     * @param {event} event
+     */
+    onSubmit(event) {
+        event.preventDefault();
+        const {
+            elements: { email, password },
+        } = event.currentTarget;
+        if (email.value === "" || password.value === "") {
+            alert("Please, fill all form fields!");
+        } else if (email.value !== "" && password.value !== "") {
+            this.logUserInfo();
+            event.currentTarget.reset();
+        } else "";
+    },
+};
+form.formElement.addEventListener("submit", form.onSubmit.bind(form));
