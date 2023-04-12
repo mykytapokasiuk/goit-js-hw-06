7cf277c6414f4e767a9681d37c338c699df99b5f
Mykyta Pokasiuk <mykyta.pokasiuk@ukr.net>
2023-04-13 02:44:18 +0300

    Task-09 is completed

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,25 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const background = {
+    bodyElement: document.querySelector("body"),
+    changeColorBtn: document.querySelector(".change-color"),
+    colorValueOutputElement: document.querySelector(".color"),
+    /**
+     * Creates random colors
+     * @returns {string} Color in hex format
+     */
+    getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215)
+            .toString(16)
+            .padStart(6, 0)}`;
+    },
+    /**
+     * Changes background color, outputs color value to the span element
+     */
+    changeBackgroundColor() {
+        this.bodyElement.style.backgroundColor = this.getRandomHexColor();
+        this.colorValueOutputElement.textContent = this.getRandomHexColor();
+    },
+};
+background.changeColorBtn.addEventListener(
+    "click",
+    background.changeBackgroundColor.bind(background)
+);
